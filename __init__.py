58090299cc05bd6734d867e0a1fb17ed041041fa
Ethan Cobb <edcobb2019@gmail.com>
2024-03-07 14:20:28 -0500

    db setup

--- __init__.py
+++ __init__.py
@@ -68,3 +68,21 @@ def test():
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
+
+
+# # LAMBDA_WARNING: Unhandled exception. The most likely cause is an 
+# issue in the function code. However, in rare cases, a Lambda runtime
+# update can cause unexpected function behavior. For functions using 
+# managed runtimes, runtime updates can be triggered by a function 
+# change, or can be applied automatically. To determine if the runtime 
+# has been updated, check the runtime version in the INIT_START log 
+# entry. If this error correlates with a change in the runtime version, 
+# you may be able to mitigate this error by temporarily rolling back to
+# the previous runtime version. For more information, 
+# see https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html
+
+# # [ERROR] Runtime.ImportModuleError: Unable to import module 'vc__
+# handler__python': No module named 'psycopg2'
+# # Traceback (most recent call last):
+
+
